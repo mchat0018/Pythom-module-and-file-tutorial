b2e4b9f60df00668e0fa2b7b839123c65ac4fe4b
Mihir Chaturvedi <mihir19175@iiitd.ac.in>
2020-06-26 16:02:51 +0530

    Reading and writing data from/to json files

--- json_mod.py
+++ json_mod.py
@@ -1,32 +1,11 @@
 import json
 
-people_string='''
-{
-    "people":[
-        {
-            "name":"John Doe",
-            "phone":"615-555-7164",
-            "emails":["johndoe69@bogusemail.com" , "john.doe@work_place_email.com"],
-            "has_license":false
-        },
-        {
-            "name":"Jane Doe",
-            "phone":"564-453-2174",
-            "emails":null,
-            "has_license":true
-        }
-    ]
-}
-'''
-data=json.loads(people_string)
-print(type(data))
-print(data)
+with open('states.json') as f:
+    data=json.load(f)
 
-for person in data["people"]:
-    print(person["name"])
+for state in data["states"]:
+    print(state["abbreviation"],state["name"])
+    del state["area_codes"]
 
-for person in data["people"]:
-    del person["phone"]   
-
-new_people_string=json.dumps(data,indent=2) 
-print(new_people_string)
+with open('new_states.json','w') as f:
+    json.dump(data,f,indent=2)
