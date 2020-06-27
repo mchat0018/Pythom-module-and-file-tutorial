ffca13cc97a91df0eca1233c303e637e31bb9280
Mihir Chaturvedi <mihir19175@iiitd.ac.in>
2020-06-27 09:11:47 +0530

    Monitoring server behaviour

--- request_mod.py
+++ request_mod.py
@@ -3,7 +3,12 @@ import requests
 #r=requests.get('https://xkcd.com/353/')
 #print(r.text) prints the html contents of the site (unicode information)
 
-r1= requests.get('https://imgs.xkcd.com/comics/python.png')
-with open('comic.png','wb') as f:
-    f.write(r1.content)  #writes binary information to the .png file
+r= requests.get('https://imgs.xkcd.com/comics/python.png')
 
+print(r.status_code) 
+#200 is a success
+#300 is redirect
+#400 is a client-error
+#500 is a server error
+print(r.ok) #will return True for anything that is less than a 400 response
+print(r.headers)    #return type=dict
