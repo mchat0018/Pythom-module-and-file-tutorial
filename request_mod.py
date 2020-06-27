fdc6375283cb5656b37101de08d1967eb6bf2d31
Mihir Chaturvedi <mihir19175@iiitd.ac.in>
2020-06-27 10:16:46 +0530

    http arguments

--- request_mod.py
+++ request_mod.py
@@ -3,12 +3,17 @@ import requests
 #r=requests.get('https://xkcd.com/353/')
 #print(r.text) prints the html contents of the site (unicode information)
 
-r= requests.get('https://imgs.xkcd.com/comics/python.png')
+payload={'page':2, 'count':5}
+r= requests.get('https://httpbin.org/get',params=payload,timeout=5)   #pass the payloa as parameters to get router information
+print(r.url)
+print(r.text)
 
-print(r.status_code) 
-#200 is a success
-#300 is redirect
-#400 is a client-error
-#500 is a server error
-print(r.ok) #will return True for anything that is less than a 400 response
-print(r.headers)    #return type=dict
+payload={'username':"Mihir",'password':"h5rt3@l"}
+r= requests.post('https://httpbin.org/post',data=payload,timeout=5)   #post a form to a router
+print(r.text)
+r_dict=r.json()     #string returned is converted to dict
+print(r_dict['form'])
+
+r=requests.get('https://httpbin.org/basic-auth/mihir/testing',auth=('mihir','testing'),timeout=5) #responds to basic authentication
+print(r)
+print(r.text)
